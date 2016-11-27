37eebd50a77f0a4ffd9f2dd5701ecd8b6af67f63
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-27 14:19:54 +0300

    Set up project

---
+++ serieztv/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  serieztv
+//
+//  Created by Goktug on 27/11/2016.
+//  Copyright © 2016 serieztv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

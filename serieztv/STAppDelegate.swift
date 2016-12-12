14335d55971c1d84f3f59b3874b7ab732f8ed263
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-12-12 16:19:31 +0300

    Add main view as root view controller

--- serieztv/STAppDelegate.swift
+++ serieztv/STAppDelegate.swift
@@ -13,14 +13,13 @@ class STAppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        let navController = UINavigationController(rootViewController: STLoginViewController())
+        let navController = UINavigationController(rootViewController: STMainViewController())
         window?.rootViewController = navController
         UIApplication.shared.statusBarStyle = .lightContent
         return true

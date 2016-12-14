634c69b78a76df8502feaa312cbc34d2e89c665a
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-12-14 14:41:32 +0300

    Initialize Home Collection View

---
+++ serieztv/Views/STHomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  STHomeCollectionViewCell.swift
+//  serieztv
+//
+//  Created by Goktug on 13/12/2016.
+//  Copyright © 2016 serieztv. All rights reserved.
+//
+
+import UIKit
+
+class STHomeCollectionViewCell: UICollectionViewCell {
+    
+}

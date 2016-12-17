64679595ec790d10c2aca34076b2cee76c25d2a3
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-12-18 00:55:11 +0300

    Initialize auth manager

---
+++ serieztv/Managers/AuthManager.swift
@@ -0,0 +1,34 @@
+//
+//  AuthManager.swift
+//  serieztv
+//
+//  Created by Goktug on 15/12/2016.
+//  Copyright © 2016 serieztv. All rights reserved.
+//
+
+import Foundation
+
+class AuthManager {
+    
+    var baseURL: String {
+        get {
+            return ""
+        }
+    }
+    
+    private init() {}
+    
+    
+    private let shared = AuthManager()
+    
+    
+    func login(username: String, password: String, completion: (_ user: User) -> ()) {
+        
+    }
+    
+    func register(email: String, username: String, password: String, completion: (_ user: User) -> ()) {
+        
+    }
+    
+    
+}

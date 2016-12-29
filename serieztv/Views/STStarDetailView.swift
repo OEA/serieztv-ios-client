58dcd05e349a01c86000964df6395a9db9b71503
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-12-29 12:06:27 +0300

    Initialize Star Detail View

---
+++ serieztv/Views/STStarDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  STStarDetailView.swift
+//  serieztv
+//
+//  Created by Goktug on 27/12/2016.
+//  Copyright © 2016 serieztv. All rights reserved.
+//
+
+import UIKit
+
+class STStarDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

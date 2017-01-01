f80399d97c1219d22bcbd5d5e256d1b7918026a2
goktugberkulu <goktug.ulu@ozu.edu.tr>
2017-01-01 03:19:54 +0300

    Add protocol to navigate detail

--- serieztv/Classes/STHomeCollectionViewController.swift
+++ serieztv/Classes/STHomeCollectionViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 private let reuseIdentifier = "HomeCell"
 
-class STHomeCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+protocol NavigateToDetailDelegate {
+    func goToDetail(vc: STDetailViewController)
+}
 
+
+class STHomeCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, NavigateToDetailDelegate {
+    
+    internal func goToDetail(vc: STDetailViewController) {
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+    let titleArray = ["Movies", "Series", "Top Rated Movies", "Top Rated Series", "Genres", "Stars"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.black
@@ -28,36 +39,36 @@ class STHomeCollectionViewController: UICollectionViewController, UICollectionVi
         self.navigationController?.navigationBar.isTranslucent = false
         self.navigationController?.navigationBar.topItem?.title = "SeriezTV"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.collectionView?.performBatchUpdates({
+            self.collectionView?.reloadData()
+        }, completion: nil)
+        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 5
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 6
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! STHomeCollectionViewCell
-        if indexPath.section % 2 == 0 {
+
+        cell.titleLabel.text = self.titleArray[indexPath.item]
+        cell.navDelegate = self
+        
+        print(self.titleArray[indexPath.item])
+        if indexPath.item % 2 == 0 {
             cell.contentView.backgroundColor = UIColor(colorLiteralRed: 26/255, green: 26/255, blue: 26/255, alpha: 1.0)
             cell.collectionView.backgroundColor = UIColor(colorLiteralRed: 26/255, green: 26/255, blue: 26/255, alpha: 1.0)
             cell.backgroundView?.backgroundColor = UIColor(colorLiteralRed: 26/255, green: 26/255, blue: 26/255, alpha: 1.0)
@@ -67,56 +78,29 @@ class STHomeCollectionViewController: UICollectionViewController, UICollectionVi
             cell.collectionView.backgroundColor = UIColor.black
             cell.contentView.backgroundColor = UIColor.black
         }
-    
-
-    
-        return cell
+        
+        if indexPath.item < 4 {
+            cell.detailCellViewIdentifier = "DetailCell"
+            cell.tag = 0
+            return cell
+        } else  if indexPath.item == 4 {
+            cell.detailCellViewIdentifier = "GenreDetailCell"
+            cell.tag = 1
+            return cell
+        } else {
+            cell.detailCellViewIdentifier = "StarDetailCell"
+            cell.tag = 2
+            return cell
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: view.frame.width, height: 220)
+        return CGSize(width: view.frame.width, height: 265)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsetsMake(0, 0, 0, 0)
     }
-    
-    
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }

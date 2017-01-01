7e5dd87c5860047576dfc99e4ded61098927c6c9
goktugberkulu <goktug.ulu@ozu.edu.tr>
2017-01-01 03:13:46 +0300

    Initialize crew detail view

---
+++ serieztv/Views/STCrewDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  STCastDetailView.swift
+//  serieztv
+//
+//  Created by Goktug on 01/01/2017.
+//  Copyright © 2017 serieztv. All rights reserved.
+//
+
+import UIKit
+
+class STCrewDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

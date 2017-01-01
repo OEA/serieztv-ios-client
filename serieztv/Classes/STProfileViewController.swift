90e1d8a76a8e83dc4233bf26549193238e347d9d
goktugberkulu <goktug.ulu@ozu.edu.tr>
2017-01-01 03:16:41 +0300

    Add table view cell functionalities

--- serieztv/Classes/STProfileViewController.swift
+++ serieztv/Classes/STProfileViewController.swift
@@ -8,32 +8,72 @@
 
 import UIKit
 
-class STProfileViewController: UIViewController {
+class STProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let profileView: STProfileView = {
         let profileView = STProfileView()
         return profileView
     }()
     
+    let titles = ["About SeriezTV", "Share SeriezTV", "Give Feedback", "Change Password", "Logout"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(profileView)
-        
+        self.profileView.tableView.delegate = self
+        self.profileView.tableView.dataSource = self
+        self.profileView.tableView.register(STProfileTableViewCell.self, forCellReuseIdentifier: "ProfileCell")
         profileView.snp.makeConstraints { (make) in
             make.leading.equalTo(0)
             make.top.equalTo(0)
             make.trailing.equalTo(0)
             make.bottom.equalTo(0)
-            
         }
-
+        self.profileView.tableView.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0);
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as! STProfileTableViewCell
+        cell.titleLabel.text = titles[indexPath.row]
+        print(titles[indexPath.row])
+        cell.selectionStyle = .gray
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (indexPath.row == 0) {
+            let aboutVC = STAboutViewController()
+            self.navigationController?.pushViewController(aboutVC, animated: true)
+        } else if (indexPath.row == 1) {
+            
+        } else if (indexPath.row == 2) {
+            
+        } else if (indexPath.row == 3) {
+            let changePasswordVC = STChangePasswordViewController()
+            self.navigationController?.pushViewController(changePasswordVC, animated: true)
+        } else if (indexPath.row == 4) {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.profileView.tableView.frame.size.height / 5
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
 

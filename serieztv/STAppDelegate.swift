22ce393b7adc59eab85f76051f9664ad43810367
Ömer Aslan <omer.aslan@ozu.edu.tr>
2017-01-03 17:11:15 +0300

    Remove managers from appdelegate

--- serieztv/STAppDelegate.swift
+++ serieztv/STAppDelegate.swift
@@ -15,128 +15,12 @@ class STAppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         let navController = UINavigationController(rootViewController: STMainViewController())
         window?.rootViewController = navController
         UIApplication.shared.statusBarStyle = .lightContent
         UINavigationBar.appearance().barTintColor = UIColor.black
-        
-        AuthManager.sharedInstance.login(username: "gbu", password: "password", completion: nil, errorCompletion: nil)
-        AuthManager.sharedInstance.register(email: "tester1@gmail", username: "tester1", password: "testp", name: "tester", completion: nil, errorCompletion: nil)
-
-        
-        MovieManager.sharedInstance.getAllMovies(completion: { (movies) in
-            print("List")
-            print(movies.count)
-            print(movies[0].name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.getTopMovies(withLimit: nil, completion: { (movies) in
-            print("Top Without Limit")
-            print(movies.count)
-            print(movies[0].name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.getTopMovies(withLimit: "3", completion: { (movies) in
-            print("Top With Limit")
-            print(movies.count)
-            print(movies[0].name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.getRecentMovies(withLimit: "3", completion: { (movies) in
-            print("Recent With Limit")
-            print(movies.count)
-            print(movies[0].name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.getRecentMovies(withLimit: nil, completion: { (movies) in
-            print("Recent Without Limit")
-            print(movies.count)
-            print(movies[0].name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.findMovieWith(id: "586a35b952f1c10688cfe375", completion: { (movie) in
-            print("Find Movie")
-            print(movie.airDate)
-            print(movie.characters[0].name)
-            print(movie.name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.searchForMovie(genre: "Action", name: nil, completion: { (movies) in
-            print(movies.count)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.searchForMovie(genre: nil, name: "Doc", completion: { (movies) in
-            print(movies.count)
-            print(movies[0].name)
-        }, errorCompletion: nil)
-        
-        MovieManager.sharedInstance.searchForMovie(genre: "Action", name: "Doc", completion: { (movies) in
-            print(movies.count)
-            print(movies[0].genres[0].name)
-        }, errorCompletion: nil)
-        
-        
-                SeriesManager.sharedInstance.getAllSeriesList(completion: { (movies) in
-                    print("List")
-                    print(movies.count)
-                    print(movies[0].name)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.getTopSeriesList(withLimit: nil, completion: { (movies) in
-                    print("Top Without Limit")
-                    print(movies.count)
-                    print(movies[0].name)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.getTopSeriesList(withLimit: "3", completion: { (movies) in
-                    print("Top With Limit")
-                    print(movies.count)
-                    print(movies[0].name)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.getRecentSeriesList(withLimit: "3", completion: { (movies) in
-                    print("Recent With Limit")
-                    print(movies.count)
-                    print(movies[0].name)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.getRecentSeriesList(withLimit: nil, completion: { (movies) in
-                    print("Recent Without Limit")
-                    print(movies.count)
-                    print(movies[0].name)
-                    print("max try \(movies[1].seasons[0].name)")
-                    print("max try \(movies[1].seasons[0].episodes[0].name)")
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.findSeriesWith(id: "586a7c7b92ac9a1abc4bedec", completion: { (movie) in
-                    print("Find Movie")
-                    print(movie)
-                    print(movie.firstAir)
-                    print(movie.characters[0].name)
-                    print(movie.name)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.searchForSeries(genre: "Action", name: nil, completion: { (movies) in
-                    print("Search Genre")
-                    print(movies.count)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.searchForSeries(genre: nil, name: "Thr", completion: { (movies) in
-                    print("Search Name")
-                    print(movies.count)
-                    print(movies[0].name)
-                }, errorCompletion: nil)
-        
-                SeriesManager.sharedInstance.searchForSeries(genre: "Action", name: "Doc", completion: { (movies) in
-                    print("Search Genre and Name")
-                    print(movies.count)
-                    print(movies[0].genres[0].name)
-                }, errorCompletion: nil)
-        
         return true
     }
     
